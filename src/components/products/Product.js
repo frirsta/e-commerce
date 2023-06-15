0dd024e23072cf2431c3f0de6a0725ccf210676f
frirsta <frirstaalikarem@MacBook-Air-som-tillhor-Frirsta.local>
2023-06-15 15:37:39 +0200

    Add css to product

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -4,12 +4,14 @@ import styles from '../../styles/Product.module.css';
 
 const Product = ({ product }) => {
   return (
+    <div className={styles.ProductSection}>
     <div className={styles.ProductContainer}>
       <Card className={styles.Product}>
         <CardMedia className={styles.Image} component={"img"} image={product.image.url} />
       </Card>
      <span className={styles.Name}>{product.name}</span> 
      <span className={styles.Prics}>{product.price.formatted_with_code}</span> 
+    </div>
     </div>
   );
 };
